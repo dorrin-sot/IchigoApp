d1d860bdb5fac5f5a7be247950df90214e879a78
dorrin-sot <dorrinsotoudeh123@gmail.com>
2025-07-01 11:06:30 +0330

    Refactor: initialize `repoMock` for FruitAncestryView tests
    This commit updates the `FruitAncestryViewInstrumentedTest` to initialize the `repoMock` using `spyk` instead of direct injection.
    
    The `repoMock` is now annotated with `@BindValue` and `@JvmField` for proper Hilt binding and initialized in the `setUp` method.
    
    A `coEvery { repoMock.fetchFruits() } coAnswers {}` stub has been added to the `fruitAncestryView_ancestryClickShouldApplyFilter` test to prevent `fetchFruits` from being called during the test.

--- app/src/androidTest/java/com/example/ichigoapp/view/fruitslist/FruitAncestryViewInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/ichigoapp/view/fruitslist/FruitAncestryViewInstrumentedTest.kt
@@ -5,14 +5,20 @@ import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
-import com.example.ichigoapp.TestMainActivity
 import com.example.ichigoapp.TestDataGenerator.generateFruit
+import com.example.ichigoapp.TestMainActivity
 import com.example.ichigoapp.model.AncestryLevel
+import com.example.ichigoapp.service.AppDatabase
+import com.example.ichigoapp.service.FruitApi
 import com.example.ichigoapp.service.Repository
 import com.example.ichigoapp.ui.theme.IchigoTheme
+import dagger.hilt.android.testing.BindValue
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
+import io.mockk.coEvery
 import io.mockk.coVerify
+import io.mockk.mockk
+import io.mockk.spyk
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -27,11 +33,19 @@ class FruitAncestryViewInstrumentedTest {
   val composeTestRule = createAndroidComposeRule(TestMainActivity::class.java)
 
   @Inject
-  lateinit var repoMock: Repository
+  lateinit var db: AppDatabase
+
+  @Inject
+  lateinit var api: FruitApi
+
+  @BindValue
+  @JvmField
+  var repoMock: Repository = mockk()
 
   @Before
   fun setUp() {
     hiltRule.inject()
+    repoMock = spyk(Repository(db, api))
   }
 
   @Test
@@ -58,6 +72,8 @@ class FruitAncestryViewInstrumentedTest {
   @Test
   fun fruitAncestryView_ancestryClickShouldApplyFilter() {
     composeTestRule.run {
+      coEvery { repoMock.fetchFruits() } coAnswers {}
+
       val fruit = generateFruit()
       setContent {
         IchigoTheme {
